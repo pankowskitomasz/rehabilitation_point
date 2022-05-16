bf318e6d0b124ac6bfdd4dabc0e2b113d2f988c6
tomaszpankowski <--get-all>
2022-05-16 14:00:27 +0200

    frontend and layout

---
+++ src/app/news/news-s1/news-s1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-news-s1',
+  templateUrl: './news-s1.component.html',
+  styleUrls: ['./news-s1.component.sass']
+})
+export class NewsS1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
